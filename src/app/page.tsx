14223bb978afe0000565face75ac30796e701de1
Roldão de Oliveira Neto <roldaoo.neto@gmail.com>
2025-11-05 11:52:44 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
+          <h1><a href="https://www.youtube.com/shorts/Ay8lynMZ4mE">Watch the video</a></h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
